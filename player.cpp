7e766c19bac599cf8c58889b29014480bb832a3d
cs2user <alveerak@gmail.com>
2018-03-05 23:40:53 -0800

    Implemented doMove and heuristic function.

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include "player.hpp"
+#include <cstdlib>
 
 /*
  * Constructor for the player; initialize everything here. The side your AI is
@@ -8,20 +9,30 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-    Board board = new Board();
-    /*
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
+    this -> board = new Board;
+    this->our_color = side;
+    if (side == BLACK)
+    {
+      this->opp_color = WHITE;
+    }
+    else
+    {
+      this->opp_color = BLACK;
+    }
 }
 
 /*
  * Destructor for the player.
  */
 Player::~Player() {
+  delete board;
 }
 
+/* Computes score for move */
+int Player::calcScore(int x, int y) {
+  //return 1;
+  return weights[y][x];
+}
 /*
  * Compute the next move given the opponent's last move. Your AI is
  * expected to keep track of the board on its own. If this is the first move,
@@ -36,9 +47,30 @@ Player::~Player() {
  * return nullptr.
  */
 Move *Player::doMove(Move *opponentsMove, int ms_left) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
+    board->doMove(opponentsMove, opp_color);
+    if (board->hasMoves(our_color))
+    {
+      int best_x = 0, best_y = 0, best_score = -1;
+      for (int x = 0; x < 8; x++) {
+        for (int y = 0; y < 8; y++){
+          Move *m = new Move(x, y);
+          /*if (board->checkMove(m, our_color)){
+            board->doMove(m, our_color);
+            return m;
+          }*/
+          if (board->checkMove(m, our_color) &&
+              calcScore(x, y) > best_score)
+          {
+            best_score = calcScore(x, y);
+            best_x = x;
+            best_y = y;
+          }
+          delete m;
+        }
+      }
+      Move *best_move = new Move(best_x, best_y);
+      board->doMove(best_move, our_color);
+      return best_move;
+    }
     return nullptr;
 }
--- player.hpp
+++ player.hpp
@@ -6,6 +6,15 @@
 #include "board.hpp"
 using namespace std;
 
+//position weights from riscos.com Figure 9.3 on Othello weightings
+static int weights [8][8] = {{7, 2, 5, 4, 4, 5, 2, 7},
+                 {2, 1, 3, 3, 3, 3, 1, 2},
+                 {5, 3, 6, 5, 5, 6, 3, 5},
+                 {4, 3, 5, 6, 6, 5, 3, 4},
+                 {4, 3, 5, 6, 6, 5, 3, 4},
+                 {5, 3, 6, 5, 5, 6, 3, 5},
+                 {2, 1, 3, 3, 3, 3, 1, 2},
+                 {7, 2, 5, 4, 4, 5, 2, 7}};
 class Player {
 
 public:
@@ -13,9 +22,14 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
-
+    int calcScore(int x, int y);
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
+
+private:
+    Side our_color;
+    Side opp_color;
+    Board *board;
 };
 
 #endif
